e97cb499d8b83bc933290c5f9d4542f96a1ca15b
pauldendulk <pauldendulk@gmail.com>
2022-02-07 09:06:58 +0100

    Writing some values to console without any understanding

---
+++ UnderstandingAsyncProgramming/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Start");
+
+Console.WriteLine("Number Of Logical Processors: {0}", Environment.ProcessorCount);
+
+ThreadPool.GetMaxThreads(out int maxWorkerThreads, out int maxCompletionPortThreads);
+Console.WriteLine($"Max Worker Threads: {maxWorkerThreads}. Max Completion Port Threads: {maxCompletionPortThreads}");
+
+ThreadPool.GetMinThreads(out int minWorkerThreads, out int minCompletionPortThreads);
+Console.WriteLine($"Min Worker Threads: {minWorkerThreads}. Min Completion Port Threads: {minCompletionPortThreads}");
+
+ThreadPool.GetAvailableThreads(out int workerThreads, out int completionPortThreads);
+Console.WriteLine($"Worker Threads: {workerThreads}. Completion Port Threads: {completionPortThreads}");
+
+Console.WriteLine("Finish");
+
+Console.ReadKey();
